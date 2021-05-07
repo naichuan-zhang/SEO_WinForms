27900be72bc942500571fbd0bb8e4aab9d6d7328
Naichuan Zhang <zhangnaichuan168@gmail.com>
2021-05-07 12:08:06 +0800

    add bat files

---
+++ CreateWebSite.bat
@@ -0,0 +1,10 @@
+﻿
+::创建应用程序池
+C:\Windows\System32\inetsrv\appcmd.exe add apppool /name:"%1" /managedRuntimeVersion:"v4.0"
+ 
+:: 删除网站
+::C:\Windows\System32\inetsrv\appcmd.exe delete site "%1"
+
+::添加网址 设置端口号为12919，网址路径不能有中文
+C:\Windows\System32\inetsrv\appcmd.exe add site /name:"%1" /bindings:"http/%2:80:www.%1,http/%2:80:%1,http/%3:80:www.%1,http/%3:80:%1"  /applicationDefaults.applicationPool:"%1" /physicalPath:"%4\%1" 
+
---
+++ StartCreate.bat
@@ -0,0 +1,17 @@
+﻿:: %1域名    %2公网ip   %3 内网ip  %4 文件夹
+
+cd C:\Users\kcai\Desktop
+
+
+
+
+call CreateWebSite.bat minneapolisksorg.org  52.170.191.187  	10.0.3.13 C:\WebSite_Four_English
+call CreateWebSite.bat iaups.org 52.170.191.187  	10.0.3.13 C:\WebSite_Four_English
+call CreateWebSite.bat MounthopeMonitor.org 52.170.191.187  	10.0.3.13 C:\WebSite_Four_English
+call CreateWebSite.bat africaafrica.org 52.170.191.187  	10.0.3.13 C:\WebSite_Four_English
+call CreateWebSite.bat RealHolidayGuides.com 52.170.191.187  	10.0.3.13 C:\WebSite_Four_English
+call CreateWebSite.bat elhaidadani93.com 52.170.191.187  	10.0.3.13 C:\WebSite_Four_English
+call CreateWebSite.bat ogpbb.com 52.170.191.187  	10.0.3.13 C:\WebSite_Four_English
+call CreateWebSite.bat digimaxpro.com 52.170.191.187 10.0.3.13  C:\WebSite_Four_English
+call CreateWebSite.bat danbursch.com 52.170.191.187 10.0.3.13 C:\WebSite_Four_English
+call CreateWebSite.bat Media68.net 104.211.10.159 	10.0.3.14 C:\WebSite_Four_English
